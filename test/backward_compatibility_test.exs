9888185062c20db6027dc02e4709c2ad3fd2b4af
JVMartyns <jvmartyns2015@gmail.com>
2026-03-27 14:54:18 -0300

    Allow nil datetimes and add legacy Ping tests

--- test/backward_compatibility_test.exs
+++ test/backward_compatibility_test.exs
@@ -78,7 +78,7 @@ defmodule BackwardCompatibilityTest do
                    board_version: 17_185,
                    board_identifier: "Tester",
                    unique_id: "TestRunner",
-                   datetime: %DateTime{}
+                   datetime: nil
                  }
                ],
                errors: []
@@ -105,7 +105,7 @@ defmodule BackwardCompatibilityTest do
                    board_version: 17_185,
                    board_identifier: "Tester",
                    unique_id: "TestRunner",
-                   datetime: %DateTime{}
+                   datetime: nil
                  }
                ],
                errors: []
@@ -132,7 +132,7 @@ defmodule BackwardCompatibilityTest do
                    board_version: 17_185,
                    board_identifier: "Tester",
                    unique_id: "TestRunner",
-                   datetime: %DateTime{}
+                   datetime: nil
                  }
                ],
                errors: []
@@ -159,7 +159,7 @@ defmodule BackwardCompatibilityTest do
                    board_version: 17_185,
                    board_identifier: "Tester",
                    unique_id: "TestRunner",
-                   datetime: %DateTime{}
+                   datetime: nil
                  }
                ],
                errors: []
@@ -186,7 +186,7 @@ defmodule BackwardCompatibilityTest do
                    board_version: 17_185,
                    board_identifier: "Tester",
                    unique_id: "TestRunner",
-                   datetime: %DateTime{}
+                   datetime: nil
                  }
                ],
                errors: []
@@ -339,7 +339,7 @@ defmodule BackwardCompatibilityTest do
                    pulses: 0,
                    value: 42,
                    kind: "bar",
-                   datetime: ~U[1970-01-01 00:00:00Z]
+                   datetime: nil
                  }
                ],
                errors: []
@@ -362,7 +362,7 @@ defmodule BackwardCompatibilityTest do
                      pulses: 0,
                      value: 42,
                      kind: "bar",
-                     datetime: ~U[1970-01-01 00:00:00Z]
+                     datetime: nil
                    }
                  ],
                  errors: []
@@ -406,6 +406,92 @@ defmodule BackwardCompatibilityTest do
       # pulses must be integer, not string
       assert {:error, %PPNet.ParseError{reason: :unknown_format}} =
                SingleCounter.parse(["bar", 42, "many", 1500])
+    end
+  end
+
+  describe "decode PPNet.Message.Ping (from v0.1.3)" do
+    test "parse/1 with valid binary data" do
+      payload =
+        <<0x04, 0x03, 0x9A, 0xDC, 0x19, 0x10, 0x53, 0xCC, 0x88, 0x72, 0x4C, 0x45, 0x7E, 0x43, 0x32, 0xCC, 0xA9, 0xCC,
+          0x8C, 0xCC, 0xE6, 0x7B, 0x20, 0x53, 0x66, 0x2C, 0xCB, 0x40, 0x39, 0x01, 0x01, 0x01, 0x01, 0x01, 0x02, 0xCF,
+          0x01, 0x06, 0x01, 0x25, 0xE7, 0x2E, 0x78, 0x1A, 0x93, 0xCB, 0x40, 0x44, 0x5B, 0x3D, 0x6F, 0x56, 0xFA, 0xE4,
+          0xCB, 0xC0, 0x52, 0x80, 0x62, 0x81, 0x9A, 0x49, 0x6D, 0xCD, 0x27, 0x10, 0xCB, 0x3F, 0xE0, 0x01, 0x01, 0x01,
+          0x01, 0x01, 0x05, 0x32, 0x64, 0x9A, 0xA7, 0x4D, 0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0xD6, 0xA7, 0x01, 0x23, 0x45,
+          0x67, 0x89, 0xAB, 0xC9, 0xA7, 0xDC, 0xFE, 0x01, 0x23, 0x45, 0x67, 0xBA, 0xA7, 0x12, 0x34, 0x56, 0x78, 0x9A,
+          0xBC, 0xBF, 0xA7, 0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6, 0xB0, 0xA7, 0x9A, 0xBC, 0xDE, 0xF0, 0x12, 0x34, 0xC4,
+          0xA7, 0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0xB5, 0xA7, 0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0xCF, 0xA7, 0xF0,
+          0xE1, 0xD2, 0xC3, 0xB4, 0xA5, 0xC6, 0xA7, 0x55, 0x44, 0x33, 0x22, 0x11, 0x1F, 0xC3, 0x92, 0xCD, 0x03, 0xE8,
+          0xCD, 0x01, 0xF4, 0x82, 0xA3, 0x66, 0x6F, 0x6F, 0xA3, 0x62, 0x61, 0x72, 0xA3, 0x62, 0x61, 0x7A, 0x7B, 0x14,
+          0xD0, 0x97, 0x92, 0x1B, 0xF1, 0x77, 0xF8, 0x00>>
+
+      assert %{
+               messages: [
+                 %Ping{
+                   session_id: "5388724c-457e-4332-a98c-e67b2053662c",
+                   temperature: 25.0,
+                   uptime_ms: 1_262_304_000_000,
+                   cpu: 0.5,
+                   location: %{lat: 40.712812345, lon: -74.006012345, accuracy: 10_000},
+                   storage: %{total: 1000, used: 500},
+                   tpu_memory_percent: 50,
+                   tpu_ping_ms: 100,
+                   wifi: [
+                     %{mac: "00:1A:2B:3C:4D:5E", rssi: -42},
+                     %{mac: "01:23:45:67:89:AB", rssi: -55},
+                     %{mac: "DC:FE:01:23:45:67", rssi: -70},
+                     %{mac: "12:34:56:78:9A:BC", rssi: -65},
+                     %{mac: "A1:B2:C3:D4:E5:F6", rssi: -80},
+                     %{mac: "9A:BC:DE:F0:12:34", rssi: -60},
+                     %{mac: "FE:DC:BA:98:76:54", rssi: -75},
+                     %{mac: "02:04:06:08:0A:0C", rssi: -49},
+                     %{mac: "F0:E1:D2:C3:B4:A5", rssi: -58},
+                     %{mac: "55:44:33:22:11:00", rssi: -61}
+                   ],
+                   datetime: nil,
+                   extra: %{"baz" => 123, "foo" => "bar"}
+                 }
+               ],
+               errors: []
+             } = PPNet.parse(payload)
+    end
+
+    test "parse/1 with valid binary data and extra is present" do
+      payload =
+        <<0x04, 0x03, 0x9A, 0xDC, 0x19, 0x10, 0x53, 0xCC, 0x88, 0x72, 0x4C, 0x45, 0x7E, 0x43, 0x32, 0xCC, 0xA9, 0xCC,
+          0x8C, 0xCC, 0xE6, 0x7B, 0x20, 0x53, 0x66, 0x2C, 0xCB, 0x40, 0x39, 0x01, 0x01, 0x01, 0x01, 0x01, 0x1D, 0xCD,
+          0x03, 0xE8, 0x93, 0xCB, 0x40, 0x44, 0x5B, 0x3D, 0x6F, 0x56, 0xFA, 0xE4, 0xCB, 0xC0, 0x52, 0x80, 0x62, 0x81,
+          0x9A, 0x49, 0x6D, 0xCD, 0x27, 0x10, 0xCB, 0x3F, 0xE0, 0x01, 0x01, 0x01, 0x01, 0x01, 0x21, 0x32, 0x64, 0x90,
+          0x92, 0xCD, 0x03, 0xE8, 0xCD, 0x01, 0xF4, 0x82, 0xA3, 0x66, 0x6F, 0x6F, 0xA3, 0x62, 0x61, 0x72, 0xA3, 0x62,
+          0x61, 0x7A, 0x7B, 0x99, 0x6C, 0xD8, 0xE6, 0x16, 0xB8, 0x18, 0x9B, 0x00>>
+
+      assert %{
+               messages: [
+                 %Ping{
+                   session_id: "5388724c-457e-4332-a98c-e67b2053662c",
+                   cpu: 0.5,
+                   location: %{accuracy: 10_000, lat: 40.712812345, lon: -74.006012345},
+                   storage: %{total: 1000, used: 500},
+                   temperature: 25.0,
+                   tpu_memory_percent: 50,
+                   tpu_ping_ms: 100,
+                   uptime_ms: 1000,
+                   wifi: [],
+                   datetime: nil,
+                   extra: %{"baz" => 123, "foo" => "bar"}
+                 }
+               ],
+               errors: []
+             } = PPNet.parse(payload)
+    end
+
+    test "parse/1 with invalid list returns error" do
+      # temperature must be a float, not an integer (legacy 9-element format)
+      assert {:error, %PPNet.ParseError{reason: :unknown_format}} =
+               Ping.parse([25, 1000, [40.0, -74.0, 100], 0.5, 50, 100, [], [1000, 500], %{}])
+
+      # extra must be a map, not a list (current 10-element format)
+      assert {:error, %PPNet.ParseError{reason: :unknown_format}} =
+               Ping.parse([[], 25.0, 1000, [40.0, -74.0, 100], 0.5, 50, 100, [], [1000, 500], ["not", "a", "map"]])
     end
   end
 end
